63ddff34ec88db8fccb21ca1716fe20e4dbeecfa
kcarollee <kcarollee31693@gmail.com>
2021-01-03 15:02:02 +0900

    add var host

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 var express = require('express');
 var app = express();
 // listen on port 3000
-var port = process.env.PORT || 3000;
-var server = app.listen(port, () =>{
+var port = process.env.PORT || 3000 || process.env.YOUR_PORT;
+var host = process.env.YOUR_HOST || '0.0.0.0';
+var server = app.listen(port, host, () =>{
   console.log('STARTING SERVER AT ${port}');
 });
 var fs = require('fs');
